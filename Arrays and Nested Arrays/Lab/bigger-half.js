764d478ba2a2b98ce94a696ac4edfade237762b3
Petar Kobakov <pkobakov@gmail.com>
2022-03-06 17:47:20 +0200

    Bigger Half Problem

---
+++ Arrays and Nested Arrays/Lab/bigger-half.js
@@ -0,0 +1,9 @@
+function bigger_half (input = []) {
+
+   let k = Math.ceil(input.length/2);
+   let sorted = input.sort((a,b)=>a-b).slice(-k);
+   return sorted;
+}
+
+bigger_half([4, 7, 2, 5]);
+bigger_half([3, 19, 14, 7, 2, 19, 6]);
