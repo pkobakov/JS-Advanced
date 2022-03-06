2feee0496ff29d987b46a0b43575a1eb4646ff00
Petar Kobakov <pkobakov@gmail.com>
2022-03-06 14:43:48 +0200

    Negative Positive Numbers Problem

---
+++ Arrays and Nested Arrays/Lab/negative-positive-numbers.js
@@ -0,0 +1,23 @@
+function negative_positive(input) {
+
+    let result = [];
+
+    for (let index = 0; index < input.length; index++) {
+       
+        let currentNum = Number(input[index]);
+
+        if (currentNum < 0) {
+             
+            result.unshift(currentNum);
+        }
+        
+        else {
+            result.push(currentNum);
+        }
+    }
+
+    console.log(result.join());
+}  
+
+negative_positive([7, -2, 8, 9]);
+negative_positive([3, -2, 0, -1]);
