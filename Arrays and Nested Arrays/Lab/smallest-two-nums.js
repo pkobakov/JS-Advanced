13107074a87ac01a3e0ce82b3edc1d399bb5d195
Petar Kobakov <69971480+pkobakov@users.noreply.github.com>
2023-09-13 00:17:59 +0300

    Delete Arrays and Nested Arrays/Lab directory

--- Arrays and Nested Arrays/Lab/smallest-two-nums.js
+++
@@ -1,9 +0,0 @@
-function smallestTwoNums(input) {
-
-    let result = input.sort((a,b) =>a-b).slice(0,2);
-    
-    console.log(result);
-}
-
-smallestTwoNums([30, 15, 50, 5]);
-smallestTwoNums([3, 0, 10, 4, 7, 3]);
