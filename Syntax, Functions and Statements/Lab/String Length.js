e59678dd9ed6b541d8eb730db6243c3942bc0a28
Petar Kobakov <pkobakov@gmail.com>
2023-09-13 00:32:45 +0300

    Added New Solution

---
+++ Syntax, Functions and Statements/Lab/String Length.js
@@ -0,0 +1,10 @@
+function StringLenght(a,b,c)
+{
+   let sumLength = a.length+b.length+c.length;
+   let averageLength = Math.floor(sumLength/3);
+   console.log(sumLength);
+   console.log(averageLength);
+}
+
+StringLenght('pasta', '5', '22.3');
+
