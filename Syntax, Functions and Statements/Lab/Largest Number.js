adb21dc1e450d4a5fff6b0f9f434ff93ce132f8d
Petar Kobakov <pkobakov@gmail.com>
2023-09-13 16:13:02 +0300

    Add New Solution

---
+++ Syntax, Functions and Statements/Lab/Largest Number.js
@@ -0,0 +1,20 @@
+function LargestNumber(a,b,c)
+{
+    let largestNumber = 0;
+
+    if(a>=b && a>=c){
+        largestNumber = a;
+    }
+
+    else if(b>=a && b>=c){
+        largestNumber = b;
+    }
+
+    else{
+        largestNumber = c;
+    }
+
+    console.log(`The largest number is: ${largestNumber}`);
+}
+
+LargestNumber(1,2,3);
