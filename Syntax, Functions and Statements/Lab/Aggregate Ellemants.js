2f4627ce9126eb414c2288897234955f26a74dde
Petar Kobakov <pkobakov@gmail.com>
2023-09-14 16:41:43 +0300

    Added New Solution

---
+++ Syntax, Functions and Statements/Lab/Aggregate Ellemants.js
@@ -0,0 +1,18 @@
+function AggregateElements(elements){
+
+    aggregate(elements, 0, (x, y) => x + y);
+    aggregate(elements, 0, (x, y) => x + 1/y);
+    aggregate(elements, '', (x, y) => x + y);
+
+
+    function aggregate(arr, initialVal, func){
+         let value = initialVal;
+         for (let i = 0; i < arr.length; i++) {          
+            value = func(value, arr[i]);
+        }
+        console.log(value);       
+    }
+}
+AggregateElements([1,2,3]);
+
+
