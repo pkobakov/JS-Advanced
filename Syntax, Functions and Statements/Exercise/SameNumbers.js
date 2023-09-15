fcbac6f6ac26bae061553e725cc655fc79964f27
Petar Kobakov <pkobakov@gmail.com>
2023-09-15 18:25:55 +0300

    Modify Solution

--- Syntax, Functions and Statements/Exercise/SameNumbers.js
+++ Syntax, Functions and Statements/Exercise/SameNumbers.js
@@ -1,10 +1,10 @@
 function sameNumbers(numbers){
    let numsAsString = numbers.toString();
-   let firstDigit = numsAsString[0];
+   let firstDigit = Number(numsAsString[0]);
    let isSame = true;
-   let sum = 0;
+   let sum = firstDigit;
 
-    for (let i = 0; i < numsAsString.length; i++) {
+    for (let i = 1; i < numsAsString.length; i++) {
         let nextDigit = numsAsString[i];
         if(firstDigit !== nextDigit){
             isSame = false;
@@ -12,14 +12,11 @@ function sameNumbers(numbers){
         } 
 
         sum += Number(numsAsString[i]);
-
-       
-
     }
 
     console.log(isSame);
     console.log(sum);
 }
-sameNumbers(2222222
-    );
+
+sameNumbers(2222222);
 sameNumbers(1234);
