19a113cfc794129de9025f179620f927047d0463
Petar Kobakov <pkobakov@gmail.com>
2023-09-17 14:49:45 +0300

    Modified Solution

--- Syntax, Functions and Statements/Exercise/Words Uppercase.js
+++ Syntax, Functions and Statements/Exercise/Words Uppercase.js
@@ -1,29 +1,6 @@
 function wordsUppercase(sentence){
 
-    // Solution 1:
-    let result = [];
-    let buffer = "";
-
-    for (let i = 0; i < sentence.length; i++) {
-        let chCode = sentence.charCodeAt(i);
-
-
-        if((chCode >= 65 && chCode <= 90) || (chCode >= 97 && chCode <= 122)){
-            buffer += sentence[i];
-        } else{
-            result.push(buffer.toUpperCase());
-            buffer = "";
-        }
-    }
-
-    if(buffer){
-        result.push(buffer.toUpperCase());
-    }
-
-    return result.toString().match(/\w+/g).join(', ');
-
-    // Solution 2:
-    //return sentence.toUpperCase().match(/\w+/g).join(', ');
+    return sentence.toUpperCase().match(/\w+/g).join(', ');
    
 }
 
