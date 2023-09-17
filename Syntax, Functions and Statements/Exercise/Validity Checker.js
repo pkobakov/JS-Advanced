a0adeb9d15d392546faaa3a580cf6a7c5a71a3f9
Petar Kobakov <pkobakov@gmail.com>
2023-09-17 14:50:14 +0300

    Added New Solution

---
+++ Syntax, Functions and Statements/Exercise/Validity Checker.js
@@ -0,0 +1,34 @@
+function validityChecker(...arr) {
+    let x1 = Number(arr[0]);
+    let y1 = Number(arr[1]);
+    let x2 = Number(arr[2]);
+    let y2 = Number(arr[3]);
+ 
+    function distance(x1, y1, x2, y2) {
+        let distH = x1 - x2;
+        let distY = y1 - y2;
+        return Math.sqrt(Math.pow(distH,2) + Math.pow(distY,2));
+    }
+ 
+    if (Number.isInteger(distance(x1, y1, 0, 0))) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+ 
+    if (Number.isInteger(distance(x2, y2, 0, 0))) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+ 
+    if (Number.isInteger(distance(x1, y1, x2, y2))) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+
+validityChecker(3, 0, 0, 4);
+validityChecker(2, 1, 1, 1);
