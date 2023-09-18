a0c6bc50bb88e9fa3b71ecd387392da2ddcab828
Petar Kobakov <pkobakov@gmail.com>
2023-09-18 15:05:22 +0300

    Added New Solution

---
+++ Arrays and Nested Arrays/Lab/Sum First Last.js
@@ -0,0 +1,14 @@
+function sumFirstLast(arr){
+    let sum = 0;
+   for (let i = 0; i < arr.length; i++) {
+    let num1 = Number(arr.shift());
+    let num2 = Number(arr.pop());
+    sum += num1 + num2;
+    
+   }
+
+   return sum;
+}
+
+sumFirstLast(['20', '30', '40']);
+sumFirstLast(['5', '10']);
