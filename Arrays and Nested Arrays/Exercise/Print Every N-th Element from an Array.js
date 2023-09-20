101394f543d3951622f88475acd7ddcae2525fa9
Petar Kobakov <pkobakov@gmail.com>
2023-09-21 00:39:59 +0300

    Added New Solution

---
+++ Arrays and Nested Arrays/Exercise/Print Every N-th Element from an Array.js
@@ -0,0 +1,25 @@
+function solve(arr, n){
+    let result = [];
+
+    for (let index = 0; index < arr.length; index+=n) {
+        
+        result.push(arr[index]);
+    }
+
+    console.log(result.join());
+}
+
+solve(['5', 
+'20', 
+'31', 
+'4', 
+'20'], 
+2
+);
+
+solve(['dsa',
+'asd', 
+'test', 
+'tset'], 
+2
+);
