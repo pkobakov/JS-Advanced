66027187fbbbefa2b3450efc8e8645825d93e6ed
Petar Kobakov <pkobakov@gmail.com>
2023-09-27 22:02:59 +0300

    Added New Solution

---
+++ Objects and Composition/Exercise/Construction Crew.js
@@ -0,0 +1,15 @@
+function asd(worker){
+
+    if(worker.dizziness === true){
+        worker.levelOfHydrated = 0.1*worker.weight*worker.experience; 
+        worker.dizziness = false;
+    }
+
+    console.log(worker);
+}
+
+asd({ weight: 80,
+      experience: 1,
+      levelOfHydrated: 0,
+      dizziness: true }
+  )
