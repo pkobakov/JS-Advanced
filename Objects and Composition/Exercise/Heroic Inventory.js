1ef330d88ecf105a94be6e3b59c4d8c142f43dd0
Petar Kobakov <pkobakov@gmail.com>
2023-09-28 00:01:23 +0300

    Added New Solution

---
+++ Objects and Composition/Exercise/Heroic Inventory.js
@@ -0,0 +1,17 @@
+function foo(input){
+   let res = [];
+
+   for (const iterator of input) {
+    let[name, level, items] = iterator.split('/');
+    level = Number(level);
+    items = items? items.split(',')  : [];
+    res.push({name, level, items});
+   }
+
+   console.log(JSON.stringify(res));
+}
+
+foo(['Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara']
+)
