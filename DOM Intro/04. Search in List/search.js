ecd39bb9a5b380c86370174b688cb126931a37f5
Petar Kobakov <pkobakov@gmail.com>
2023-10-01 17:03:13 +0300

    Added New Solution

---
+++ DOM Intro/04. Search in List/search.js
@@ -0,0 +1,23 @@
+function search() {
+   let searchField = document.getElementById('searchText').value;
+   let towns = Array.from(document.querySelectorAll('#towns li'));
+   let counter = 0;
+
+   towns.forEach(town => {
+      
+      if (town.textContent.includes(searchField)) {
+         
+         town.style.fontWeight = 'bold';
+         town.style.textDecoration = 'underline';
+
+         return counter++;
+      } 
+
+      town.style.fontWeight = 'normal';
+      town.style.textDecoration = 'none'
+   });
+
+   document.getElementById('result').textContent = `${counter} matches found`;
+   
+   
+}
