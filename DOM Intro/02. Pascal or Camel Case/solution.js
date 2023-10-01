5621acc8518e21f004032954c157d62a61d7d6b5
Petar Kobakov <pkobakov@gmail.com>
2023-10-01 11:12:42 +0300

    Added New Solution

---
+++ DOM Intro/02. Pascal or Camel Case/solution.js
@@ -0,0 +1,30 @@
+function solve() {
+  let text = document.getElementById('text').value;
+  let convention = document.getElementById('naming-convention').value;
+  let res  = "";
+  
+  let textRefact = text.toLocaleLowerCase().split(' ');
+
+  
+
+if(convention === 'Camel Case'){
+
+   res += textRefact.shift();
+   textRefact.forEach(element => {
+       res += element[0].toUpperCase() + element.substring(1)
+   });
+}
+  else if (convention === 'Pascal Case') {
+    
+    textRefact.forEach(element => {
+      res += element[0].toUpperCase() + element.substring(1)
+  });
+
+} else {
+    res = 'Error';
+  }
+  
+  document.getElementById('result').textContent = res;
+  
+}
+
