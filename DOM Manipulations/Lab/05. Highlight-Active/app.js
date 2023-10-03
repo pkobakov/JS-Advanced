6c74a0d22693ee9dafef4e3833bd9e318099c995
Petar Kobakov <pkobakov@gmail.com>
2023-10-03 16:28:49 +0300

    Added New Solution

---
+++ DOM Manipulations/Lab/05. Highlight-Active/app.js
@@ -0,0 +1,15 @@
+function focused() {
+    let inputs = document.getElementsByTagName('input');
+
+    for (const input of inputs) {
+        input.addEventListener('focus', (event)  => {
+            event.target.parentElement.classList.add('focused')
+        });
+        input.addEventListener('blur', (event) => {
+            event.target.parentElement.classList.remove('focused')
+        });
+    }
+
+
+
+}
