cb78e4c6b2f35298ed5d4af42fd0fbc2ab30934c
Petar Kobakov <pkobakov@gmail.com>
2023-10-04 01:25:44 +0300

    Added New Solution

---
+++ DOM Manipulations/Exercise/01. Sections/script.js
@@ -0,0 +1,21 @@
+function create(words) {
+   let divElement = document.getElementById('content');
+   
+   for(let word of words) 
+   {
+      let newParagraph = document.createElement('p');
+      newParagraph.textContent = word;
+      newParagraph.style.display = 'none';
+
+      let newDiv = document.createElement('div');
+      newDiv.appendChild(newParagraph);
+      newDiv.addEventListener('click', hiddenParagraph)
+      
+      divElement.appendChild(newDiv);
+   }
+
+   function hiddenParagraph(){
+      let currentP = this.children[0];
+      currentP.style.display = 'inline';
+   }
+}
