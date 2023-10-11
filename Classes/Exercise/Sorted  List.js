af87805c1427caf6326436b04d0b88d4616de7d0
Petar Kobakov <pkobakov@gmail.com>
2023-10-11 17:57:10 +0300

    Added New Solution

---
+++ Classes/Exercise/Sorted  List.js
@@ -0,0 +1,43 @@
+class List {
+    constructor(){
+       this.list = [];
+       this.size = this.list.length;
+    }
+
+    add(input){
+        this.list.push(Number(input));
+        this.list.sort((a,b) => a - b);
+        this.size = this.list.length;
+
+    }
+
+    remove(index){
+      if (index < 0 || index > this.size) {
+        throw new Error('Index is out of range');
+      }
+      
+      this.list.splice(index,1);
+      this.size = list.length;
+      this.list.sort((a,b) => a - b);
+      return this.list;
+      
+    }
+
+    get(index){
+        if (index < 0 || index > this.size) {
+            throw new Error('Index is out of range');
+        }
+          
+        this.size = list.length;
+        this.list.sort((a,b) => a - b);
+        return this.list[index];
+    }
+}
+
+let list = new List();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1)); 
+list.remove(1);
+console.log(list.get(1));
--- Classes/Exercise/Tickets.js
+++ Classes/Exercise/Tickets.js
@@ -1,4 +1,3 @@
-
 function solve(arr, criteria){
 
     class Ticket {
